5b61ec42c3ee90c4de124b61d9c2e9b32eb21368
ShizukuKiri <kira.kira@live.dk>
2020-08-06 00:14:44 +0200

    updated

--- DatabaseFirst/Program.cs
+++ DatabaseFirst/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+
+
 
 namespace DatabaseFirst
 {
@@ -10,9 +13,16 @@ namespace DatabaseFirst
     {
         static void Main(string[] args)
         {
+
+            DataBaseFirstEntities1 dataBase = new DataBaseFirstEntities1();
+
+            Console.WriteLine($" Route / Depature for {dataBase.Airports.Find(dataBase.Routes.Find(1).DestinationAirportID).IATA} Owned by {dataBase.FlightCompanies.Find(dataBase.Routes.Find(1).RouteOwnerID).Name} Operated by {dataBase.FlightCompanies.Find(dataBase.Routes.Find(1).RouteUserID).Name}");
+            // Route too --> Airport IATA --> owned by Airport FlightCompany --> Operated by FlightCompany
+
+            Console.ReadKey();
+
+
         }
     }
 }
 
-//prof of it working
-// foreach
